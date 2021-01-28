96d12e950201e9b997e78932bd228312407b23b1
FAL <falworks.contact@gmail.com>
2021-01-28 23:00:54 +0900

    feat: allow RegExp

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ const PLUGIN_NAME = "global-externals";
  * Object that contains a filter and a mapping function for replacing modules
  * with corresponding global variables.
  */
-type GlobalsMapper = {
+export type GlobalsMapper = {
   /**
    * Regular expression that match module paths to be processed by this plugin.
+   *
+   * @see <https://esbuild.github.io/plugins/>
    */
   modulePathFilter: RegExp;
 
@@ -16,25 +18,18 @@ type GlobalsMapper = {
    * Function that returns a global variable name with which the import
    * statements of `modulePath` should be replaced.
    */
-  getVariableName: (modulePath: string) => string | undefined;
+  getVariableName: (modulePath: string) => string;
 };
 
 /**
- * Creates a mapper object from an object-form table.
+ * Create a `Plugin` for replacing modules with corresponding global variables.
+ *
+ * @param globals See type declaration.
  */
-const fromTable = (globals: Record<string, string>): GlobalsMapper => ({
-  modulePathFilter: new RegExp(`^(?:${Object.keys(globals).join("|")})$`),
-  getVariableName: (modulePath: string) => globals[modulePath],
-});
-
-/**
- * @param globals
- * Mapping from module paths to variable names, e.g. `jquery: "$"`.
- */
-export const globalExternals = (
-  globals: Record<string, string>
+export const globalExternalsWithRegExp = (
+  globals: GlobalsMapper
 ): esbuild.Plugin => {
-  const { modulePathFilter, getVariableName } = fromTable(globals);
+  const { modulePathFilter, getVariableName } = globals;
 
   return {
     name: PLUGIN_NAME,
@@ -44,15 +39,30 @@ export const globalExternals = (
         namespace: PLUGIN_NAME,
       }));
 
-      build.onLoad({ filter: /.*/, namespace: PLUGIN_NAME }, (args) => {
-        const variableName = getVariableName(args.path);
-        if (!variableName) {
-          // Shouldn't happen
-          throw new Error(`Unknown module path: ${args.path}`);
-        }
-
-        return { contents: `export default ${variableName};` };
-      });
+      build.onLoad({ filter: /.*/, namespace: PLUGIN_NAME }, (args) => ({
+        contents: `export default ${getVariableName(args.path)};`,
+      }));
     },
   };
 };
+
+/**
+ * Creates a mapper object from an object-form table.
+ */
+const mapperFromTable = (globals: Record<string, string>): GlobalsMapper => ({
+  modulePathFilter: new RegExp(`^(?:${Object.keys(globals).join("|")})$`),
+  getVariableName: (modulePath: string) => globals[modulePath] as string,
+});
+
+/**
+ * Create a `Plugin` for replacing modules with corresponding global variables.
+ *
+ * @param globals Object that maps module paths to variable names, e.g.:
+ *   ```
+ *   const globals = { jquery: "$" };
+ *   const plugins = [globalExternals(globals)];
+ *   ```
+ */
+export const globalExternals = (
+  globals: Record<string, string>
+): esbuild.Plugin => globalExternalsWithRegExp(mapperFromTable(globals));
